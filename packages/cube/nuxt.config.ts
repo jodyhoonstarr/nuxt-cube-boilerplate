303035cd7baaf236f0b6785af5cb818a376c7424
Jody Hoon-Starr <jody@protonmail.com>
2024-05-22 15:29:24 -0400

    feat: rm css utils

--- packages/cube/nuxt.config.ts
+++ packages/cube/nuxt.config.ts
@@ -1,31 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const glob = require('glob');
-const path = require('path');
-
-
-const relativeCss = (relativePath) => {
-  const d = path.resolve(__dirname, relativePath);
-  return glob.sync(path.join(d, '**/*.css'));
-}
-
-const absoluteCss = (absolutePath) => {
-  return glob.sync(path.join(absolutePath, '**/*.css'));
-}
-
-const findCssInPath = (path) => {
-  if (path.startsWith('/')) {
-    return absoluteCss(path);
-  } else {
-    return relativeCss(path);
-  }
-}
-
-const relativeCssFile = (relativePath) => {
-  return path.resolve(__dirname, relativePath);
-}
-
-const globalCssFile = relativeCssFile('./src/css/global.css');
-
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   telemetry: false,
@@ -38,14 +12,4 @@ export default defineNuxtConfig({
     },
   },
   modules: ['@nuxtjs/tailwindcss'],
-  // vite: {
-  //   css: {
-  //     preprocessorOptions: {
-  //       postcss: {
-  //         additionalData: `@use "${globalCssFile}" as *\n`
-  //       }
-  //     }
-  //   }
-  // }
-  
 })
