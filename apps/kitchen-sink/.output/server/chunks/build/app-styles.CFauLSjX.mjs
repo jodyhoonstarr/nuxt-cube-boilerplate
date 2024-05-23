a4fe51a8a05e4f392c496351f943bd6d7575d7f6
Jody Hoon-Starr <jody@protonmail.com>
2024-05-23 17:28:19 -0400

    chore: rm .output

--- apps/kitchen-sink/.output/server/chunks/build/app-styles.CFauLSjX.mjs
+++
@@ -1,6 +0,0 @@
-import { a as app_vue_vue_type_style_index_0_lang } from './entry-styles-4.mjs-DZlvqjf6.mjs';
-
-const appStyles_CFauLSjX = [app_vue_vue_type_style_index_0_lang];
-
-export { appStyles_CFauLSjX as default };
-//# sourceMappingURL=app-styles.CFauLSjX.mjs.map
